ca412fb9ccb23276338652e2220c082ef9354a71
Sunny Qi <sunny_qi@Sunnys-MBP-2.fios-router.home>
2020-04-17 00:03:40 -0400

    connected uploads to file picker, but upload isn't completely implemented yet

--- app/src/main/java/cs3220/project/wifidirectp2p/FileUploadsActivity.java
+++ app/src/main/java/cs3220/project/wifidirectp2p/FileUploadsActivity.java
@@ -7,16 +7,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class FileUploadsActivity extends AppCompatActivity {
+
+    ArrayList<String> names = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_uploads);
+        File[] uploads = getApplicationContext().getFilesDir().listFiles();
+        if(uploads != null) {
+            for (File f : uploads) {
+                names.add(f.getName());
+            }
+        }
     }
 
     public void onUploadClick(View view) {
         Log.i("FileUploadsActivity","Uploading File");
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        startActivity(intent);
     }
 
     public void onBackClick(View view) {
